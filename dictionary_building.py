bba0784bd2e6b5227738d7f681e75884535fe8a7
Hans Bjerkevoll <hansbjerkevoll@gmail.com>
2018-10-27 00:37:00 +0200

    task 2. 2.0 - 2.2

---
+++ dictionary_building.py
@@ -0,0 +1,29 @@
+import gensim
+
+"""
+    Task 2: Dictionary building
+        Remove stopwords and convert paragraphs into Bags-of-Words(?)
+"""
+
+def build_dictionary(paragraphs):
+
+    # 2.0 Build a dictionary
+    dictionary = gensim.corpora.Dictionary(paragraphs)
+
+    # 2.1 Filter out stopwords using list from shady internet place
+    stopwords = open("txt_files/common-english-words.txt", "r").readline().split(',')
+    stopwords_indexes = dictionary.doc2idx(stopwords)
+    stopwords_id = list()
+
+    for i, stopword in enumerate(stopwords):
+        if stopwords_indexes[i] != -1:
+            stopwords_id.append(dictionary.token2id[stopword])
+
+    dictionary.filter_tokens(stopwords_id)
+
+    # 2.2 Map paragraphs into Bags-Of-Words. A corpus object
+    corpus = [dictionary.doc2bow(paragraph) for paragraph in paragraphs]
+    return corpus
+
+
+
--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import data_loading_preprocessing
+import dictionary_building
 
 """
     Task 1: Data loading and preprocessing
 """
-processed_paragraphs = data_loading_preprocessing.load_and_process_file("practice.txt")[0]
-original_paragraphs = data_loading_preprocessing.load_and_process_file("practice.txt")[1]
+paragraphs = data_loading_preprocessing.load_and_process_file("pg3300.txt")
+processed_paragraphs = paragraphs[0]
+original_paragraphs = paragraphs[1]
 
+"""
+    Task 2: Dictionary building
+"""
+dictionary_building.build_dictionary(processed_paragraphs)
 
 
 
